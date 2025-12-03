9b0c76500decba954fc598917db6db620a8c1f30
ashlynntevyaw <ashtevyaw828@gmail.com>
2025-12-03 17:25:30 -0500

    (coffee_sales_head_data).R

--- (coffee_sales_head_data).R
+++ (coffee_sales_head_data).R
@@ -1,14 +1,8 @@
-library(readr)
-library(ggplot2)
-library(tidyr)
-library(dplyr)
-library(knitr)
-
-
-coffee <- read_csv("Coffee_sales.csv")
+coffee <- read.csv("Coffee_sales.csv")
 
 head(coffee)
 
 
 options(tibble.width = Inf)
+
 head(coffee, 20) 
